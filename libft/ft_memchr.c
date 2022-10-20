7dcde84098f5c694ad1fea084a7f6b383dbe3f1f
ncornacc <ncornacc@student.codam.nl>
2022-10-20 20:49:42 +0200

    fized memchr anc makefile relik B

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ncornacc <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 08:19:11 by ncornacc      #+#    #+#                 */
-/*   Updated: 2022/10/09 17:42:09 by ncornacc      ########   odam.nl         */
+/*   Updated: 2022/10/20 20:43:17 by ncornacc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	ch;
 	unsigned char	*ptr;
 	unsigned char	*find;
 
 	ptr = (unsigned char *)s;
-	ch = (unsigned char)c;
 	find = (void *)s;
 	i = 0;
 	while (i < n)
